9ba71aabc3e10392a1c8d22a52c0918a24f8c642
nathanrhysowen <nathanrhysowen@gmail.com>
2023-07-03 15:57:20 +0100

    finished model of piano

--- src/lib/Key.js
+++ src/lib/Key.js
@@ -2,15 +2,27 @@ import * as THREE from 'three';
 
 export default class Key {
     constructor(keyName, xOffset){ //assigning two parameters to the constructor 
+        this.keyName = keyName; // assigning the keyName parameter to the keyName property 
         this.keyGroup = new THREE.Group(); //creating a group
         
-        this.keyName = keyName; // assigning the keyName parameter to the keyName property 
-        let geometry = new THREE.BoxGeometry(9, 40, 4) // creating the geometry for the natural keys
-        let material = new THREE.MeshNormalMaterial(); //creating the material to colour the object
-        this.mesh = new THREE.Mesh(geometry, material); //creating the mesh using the geometry and material i just created
-        this.mesh.position.x = xOffset; //moving the mesh along the x axis
-
-        this.keyGroup.add(this.mesh); //transforming the mesh into a child of the keyGroup
+        if (keyName.length === 3){
+           // is flat key 
+           let geometry = new THREE.BoxGeometry(6, 24, 4); // creating the geometry for the natural keys
+           let material = new THREE.MeshStandardMaterial({ color: 0x000000 }); //creating the material to colour the object
+           this.mesh = new THREE.Mesh(geometry, material); //creating the mesh using the geometry and material i just created
+           this.mesh.position.x = xOffset; //moving the mesh along the x axis
+           this.mesh.position.y = 8;
+           this.mesh.position.z = 4;
+           this.keyGroup.add(this.mesh);
+        } else {
+            // is natural key
+            let geometry = new THREE.BoxGeometry(9, 40, 4) // creating the geometry for the natural keys
+            let material = new THREE.MeshStandardMaterial({ color: 0xfafafa }); //creating the material to colour the object
+            this.mesh = new THREE.Mesh(geometry, material); //creating the mesh using the geometry and material i just created
+            this.mesh.position.x = xOffset; //moving the mesh along the x axis
+            this.keyGroup.add(this.mesh);
+        }
+        //transforming the mesh into a child of the keyGroup
     }
     getKeyGroup() {
         return this.keyGroup //allowing external code to get a reference to the keyGroup 
--- src/lib/Piano.js
+++ src/lib/Piano.js
@@ -5,13 +5,26 @@ export default class Piano {
     constructor(){
         this.pianoGroup = new THREE.Group();
 
-        //let geometry = new THREE.BoxGeometry(16, 24, 24);
-        //let material = new THREE.MeshNormalMaterial();
-        //let mesh = new THREE.Mesh(geometry, material);
-
-        //this.pianoGroup.add(mesh);
+        
         this.addNaturalKeys();
+        this.addFlatKeys();
 
+    }
+
+    addFlatKeys(){
+        let flatKeys = [
+            {name: 'Db4', xOffset: 5 },
+            {name: 'Eb4', xOffset: 15 },
+            {name: 'Gb4', xOffset: 35 },
+            {name: 'Ab4', xOffset: 45 },
+            {name: 'Bb4', xOffset: 55 },
+            
+        ];
+
+        flatKeys.forEach((flatKey) => {
+            let key = new Key(flatKey.name, flatKey.xOffset);
+            this.pianoGroup.add(key.getKeyGroup());
+        });
     }
 
     addNaturalKeys() {
@@ -19,6 +32,10 @@ export default class Piano {
         {name: 'C4', xOffset: 0},
         {name: 'D4', xOffset: 10},
         {name: 'E4', xOffset: 20},
+        {name: 'F4', xOffset: 30},
+        {name: 'G4', xOffset: 40},
+        {name: 'A4', xOffset: 50},
+        {name: 'B4', xOffset: 60},
       ];
       naturalKeys.forEach((naturalKey) => {
         let key = new Key(naturalKey.name, naturalKey.xOffset);
