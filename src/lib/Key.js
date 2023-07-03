ad440ab89f6fc7918773d7525ac8e4151cd3c40a
nathanrhysowen <nathanrhysowen@gmail.com>
2023-07-03 15:07:20 +0100

    Adding comments to the key file

--- src/lib/Key.js
+++ src/lib/Key.js
@@ -1,18 +1,18 @@
 import * as THREE from 'three';
 
 export default class Key {
-    constructor(keyName, xOffset){
-        this.keyGroup = new THREE.Group();
+    constructor(keyName, xOffset){ //assigning two parameters to the constructor 
+        this.keyGroup = new THREE.Group(); //creating a group
         
-        this.keyName = keyName;
-        let geometry = new THREE.BoxGeometry(9, 40, 4)
-        let material = new THREE.MeshNormalMaterial();
-        this.mesh = new THREE.Mesh(geometry, material);
-        this.mesh.position.x = xOffset;
+        this.keyName = keyName; // assigning the keyName parameter to the keyName property 
+        let geometry = new THREE.BoxGeometry(9, 40, 4) // creating the geometry for the natural keys
+        let material = new THREE.MeshNormalMaterial(); //creating the material to colour the object
+        this.mesh = new THREE.Mesh(geometry, material); //creating the mesh using the geometry and material i just created
+        this.mesh.position.x = xOffset; //moving the mesh along the x axis
 
-        this.keyGroup.add(this.mesh);
+        this.keyGroup.add(this.mesh); //transforming the mesh into a child of the keyGroup
     }
     getKeyGroup() {
-        return this.keyGroup
+        return this.keyGroup //allowing external code to get a reference to the keyGroup 
     }
 }
